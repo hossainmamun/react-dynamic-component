2c36bc9740bd1b2af8d732ea8598e5409f4c87fb
hossainmamun <hossainmamun337@gmailcom>
2021-04-03 00:13:16 +0600

    dynamic component commit

--- src/App.js
+++ src/App.js
@@ -2,24 +2,61 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  // declare array of object..
+  const playerProfile = [
+    {name: "messi", club: "barselona", position: "mid-fielder", netIncome: "$1000000"},
+    {name: "roneldo", club: "jubentus", position: "winger", netIncome: "$2000000"},
+    {name: "sadio-mane", club: "liverpool", position: "forward", netIncome: "$50000"},
+    {name: "md sala", club: "liverpool", position: "mid-fielder", netIncome: "$80000"},
+    {name: "masut ozil", club: "arsenal", position: "mid-fielder", netIncome: "$900000"},
+    {name: "neymer jr", club: "psg", position: "mid-fielder", netIncome: "$800000"},
+    {name: "d-maria", club: "manchester city", position: "mid-fielder", netIncome: "$500000"},
+  ]
   return (
     <div className="App">
+      <h2>dynamic component form an array of object</h2>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          {
+            // map on playerProfile..
+            playerProfile.map(player => <Player profile={player}></Player>)
+          }
       </header>
     </div>
   );
+}
+
+
+// declare component..
+function Player(props){
+  // object destructuring form array of object
+  const {name, club, position, netIncome} = props.profile;
+  
+  // apply style..
+  const playerStyle = {
+    backgroundColor: "#2E8B57",
+    padding: "10px",
+    margin: "10px",
+
+  }
+  // apply style for button
+  const btnStyle = {
+    color: "#fff",
+    backgroundColor: "#008000",
+    padding: "10px",
+    fontSize: "18px",
+    textTransform: "capitalize",
+    border: "none",
+    cursor: "pointer",
+  }
+  return (
+    <div style = {playerStyle}>
+      <h4>name: {name}</h4>
+      <h5>club: {club}</h5>
+      <p>position: {position}</p>
+      <p>net-income: {netIncome}</p>
+      <button style = {btnStyle}>learn more</button>
+    </div>
+  )
 }
 
 export default App;
